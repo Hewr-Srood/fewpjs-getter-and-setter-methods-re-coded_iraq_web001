3d3972b2b893694adc356c5aa41a7bdee79bfbe3
Hewr Srood <hewr_s@outlook.com>
2020-06-22 08:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ class Circle {
   constructor(radis) {
     this.radius=radius;
   }
-  set diameter(){
+  set diameter(radius){
     this.diameter= this.radius *2;
   }
   get diameter(){
     return this.diameter;
   }
-  set circumference(){
+  set circumference(diameter){
     this.circumference= Math.PI *this.diameter;
   }
   get circumference(){
     return this.circumference;
   }
-  set area(){
+  set area(radis){
     this.area=Math.PI * Math.pow(this.radius, 2);
   }
   get area (){
