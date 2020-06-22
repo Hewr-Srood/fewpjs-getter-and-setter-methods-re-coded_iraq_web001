bf2aaab6ad51f4db84f2b22b9c385cd52b7bf589
Hewr Srood <hewr_s@outlook.com>
2020-06-22 08:20:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+class Circle {
+  constructor(radis) {
+    this.radius=radius;
+  }
+  set diameter(){
+    this.diameter= this.radius *2;
+  }
+  get diameter(){
+    return this.diameter;
+  }
+  set circumference(){
+    return
+  }
+}
