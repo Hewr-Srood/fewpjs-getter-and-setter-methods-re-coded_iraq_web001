1f028787125aba0c2d55ccd6bc1ddfd3bc8a461f
Hewr Srood <hewr_s@outlook.com>
2020-06-22 08:42:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 // Add your Circle class here
-class Circle {
-  constructor(radius) {
-    this.radius=radius;
-  }
 
-  set diameter(diameter){
-    this.diameter= diameter;
+class Circle {
+  constructor(radius){
+    this.radius = radius;
   }
   get diameter(){
-    return (this.diameter||( (this.radius) *2));
-  }
-  set circumference(circumference){
-    this.circumference=circumference;
+    return this.radius * 2
   }
   get circumference(){
-    return  Math.PI *this.diameter || this.circumference;
+    return Math.PI * this.diameter
+  }
+  get area(){
+    return (this.radius ** 2) * Math.PI
+  }
+  set diameter(diameter){
+    this.radius = diameter / 2
+  }
+  set circumference(circumference){
+    this.diameter = circumference / Math.PI
   }
   set area(area){
-    this.area=area;
-  }
-  get area (){
-    return (this.area ||(Math.PI * Math.pow(this.radius, 2)));
+    this.radius = Math.sqrt(area / Math.PI)
   }
 }
+// class Circle {
+//   constructor(radius) {
+//     this.radius=radius;
+//   }
+// 
+//   set diameter(diameter){
+//     this.diameter= diameter;
+//   }
+//   get diameter(){
+//     return (this.diameter||( (this.radius) *2));
+//   }
+//   set circumference(circumference){
+//     this.circumference=circumference;
+//   }
+//   get circumference(){
+//     return  Math.PI *this.diameter || this.circumference;
+//   }
+//   set area(area){
+//     this.area=area;
+//   }
+//   get area (){
+//     return (this.area ||(Math.PI * Math.pow(this.radius, 2)));
+//   }
+// }
